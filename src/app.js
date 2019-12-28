b3d46f98e2afb54587a0e0ef2fbc8cf85f07a190
Siddhant Jain <sidsanjeevjain@gmail.com>
2019-12-29 02:23:41 +0530

    Init commmit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+const yargs = require('yargs')
+const notes = require('./notes.js')
+yargs.command({
+    command: "add",
+    describe: "Adding a note!",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title,argv.body)
+    }
+})
+yargs.command({
+    command: "remove",
+    describe: "Removing a note!",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+})
+yargs.command({
+    command: 'list',
+    describe: 'List notes!',
+    handler(){
+        notes.listNotes()
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note!',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
+    }
+})
+yargs.parse()
